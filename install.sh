6931f38a76c07fdd9621600b6c99d1a19ae306e0
Ben Klein <robobenklein@gmail.com>
2018-04-03 00:09:24 -0400

    Update install.sh
    #14

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@ echo "This is the Open-Airplay installer for Ubuntu and other debian-based syste
 sleep 1
 echo "We are about to install required Python libraries, please enter your password when prompted."
 sleep 2
-echo "sudo apt-get install libavahi-compat-libdnssd-dev python3 python3-pyqt4 python3-qt4"
-sudo apt-get install libavahi-compat-libdnssd-dev python3 python3-pyqt4 python3-qt4
+echo "sudo apt-get install libavahi-compat-libdnssd-dev python3 python3-pyqt4 python3-qt4 python3-zeroconf"
+sudo apt-get install libavahi-compat-libdnssd-dev python3 python3-pyqt4 python3-qt4 python3-zeroconf
 echo "Required system packages have been installed."
 echo "You can now run the program using run.sh."
